a101fe3d784734c36a90184893e0d96e58f6eb69
Sourabh Mandal <sourabhmandal008@gmail.com>
2023-01-05 23:09:22 +0530

    primenumber

---
+++ temp.js
@@ -0,0 +1,20 @@
+Problem 2 : Check whether a string is palindrome or not.
+
+for(let i=1; i<=10; i++){
+  console.log(i);
+
+  let num= i;
+  let count=0;
+  for(let j=1; j<=num; j--){
+    if(num%j==0){
+      count++;
+    }
+  }
+  if (count==2){
+    console.log("Prime number")
+  }
+  else{
+    console.log("Not a prime number")
+  }
+    
+}
